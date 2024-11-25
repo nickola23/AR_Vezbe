2fef0d73b8988e69b7be279ecaabf1181ecb806d
nickola23 <nikolamilanovic2305@gmail.com>
2024-11-25 13:54:50 +0100

    first commit

---
+++ T34/v05/nzd_gl.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int nzd(int a, int b);
+
+int main()
+{
+    int a=8;
+    int b=12;
+    int rez;
+    rez=nzd(a, b);
+    printf("NZD za %d i %d je %d\n",a, b, rez);
+}
---
+++ T34/v05/saberigl.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int saberi(int a, int b);
+
+int main()
+{
+    int rez;
+    rez = saberi(100, 16);
+    printf("zbir 100 i 16 je %d\n", rez);
+}
